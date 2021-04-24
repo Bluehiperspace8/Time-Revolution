f77b599a283eb5a3eac6e73aef7804ef209db5b1
RobertVJ <43622574+RobertVJ@users.noreply.github.com>
2021-04-24 13:04:23 -0500

    Nivel III objectos Listos
    Dialogos de cada objecto agregado

---
+++ Assets/Scripts/Puzzles/Nivel3/ObjectosMuseo/ObjectosMuseoInt/Objecto14Museo.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+/*
+ Objetivo: Dialogo de Jacob al encontrar el 14/14 objectos en el museo
+ Autor: Roberto Valdez Jasso
+ */
+
+public class Objecto14Museo : MonoBehaviour
+{
+    // Variables ---//
+    // llamanndo al mensaje
+    public TextMeshProUGUI textD;
+
+    [TextArea(3, 30)]
+    // String para el Texto (Arreay de Parrafos)
+    public string[] parrafos;
+
+    // Index
+    int index;
+
+    //Velocidad del Parrafo
+    public float velParrafo;
+
+    // GameObjects----//
+    // Botones
+    // Boton Continuar 
+    public GameObject botonContinuar;
+
+    // Boton Quitar
+    public GameObject botonQuitar;
+
+    // Panel Dialogo
+    public GameObject PanelDialogo;
+    // Boton Lectura
+    public GameObject BotonLeer;
+
+    // Referencia al auido Source
+    public AudioSource EfectoSonido;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Declarando el inicio
+        //No estara prendido hasta que el objeto sea utilizado
+        botonQuitar.SetActive(false);
+        BotonLeer.SetActive(false);
+        PanelDialogo.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Si utilizamos el objecto pasamos al if
+
+        if (textD.text == parrafos[index])
+        {
+            botonContinuar.SetActive(true);
+        }
+    }
+
+    // Corutina
+
+    IEnumerator TextDialogo()
+    {
+        foreach (char letra in parrafos[index].ToCharArray())
+        {
+
+            textD.text += letra;
+
+            yield return new WaitForSeconds(velParrafo);
+        }
+    }
+
+    // Funcion
+    // Manejo de los controles
+    public void siguienteParrafo()
+    {
+        botonContinuar.SetActive(false);
+        if (index < parrafos.Length - 1)
+        {
+            index++;
+            textD.text = "";
+            StartCoroutine(TextDialogo());
+        }
+        else
+        {
+            textD.text = "Has encontrado el objecto 14 de 14";
+            botonQuitar.SetActive(true);
+
+        }
+    }
+
+    //Interaccion  con el jugador
+    public void OnTriggerEnter2D(Collider2D collsion)
+    {
+        if (collsion.CompareTag("Player"))
+        {
+            BotonLeer.SetActive(true);
+
+        }
+        else
+        {
+            BotonLeer.SetActive(false);
+
+        }
+    }
+
+    public void activarBotonLeer()
+    {
+        PanelDialogo.SetActive(true);
+        EfectoSonido.Play();
+        StartCoroutine(TextDialogo());
+    }
+
+    public void botonCerrar()
+    {
+        PanelDialogo.SetActive(false);
+        BotonLeer.SetActive(false);
+        botonQuitar.SetActive(false);
+        textD.text = "";
+        Destroy(gameObject, t: 0.1f);
+
+    }
+}
